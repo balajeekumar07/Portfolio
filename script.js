77869a997d14803a8959124aa24f0434397668fa
Balajee Kumar <84266677+balajeekumar07@users.noreply.github.com>
2025-03-10 16:57:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,5 @@
 
 
-var typed = new Typed(".typewriter-text",
-    {
-        Strings: [" ",
-            "Web Developer",
-
-            "Front End Developer",
-
-            "Graphics Designer",
-
-            "Logo Designer"
-        ],
-        typespeed:100,
-        backspeed:70,
-        loop:true
-        // showCursor:false,
-        // cursorChar:"_",
-    }
-)
-
 
 function showSidebar(){
     const sidebar = document.querySelector('.sidebar')
